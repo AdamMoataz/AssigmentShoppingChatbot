084419004ebab313ea8b3fc0329da4331b5e50af
Adam Moataz <adam.moataz.hindawi2014@gmail.com>
2024-05-30 20:57:30 +0300

    I did the assignment for shopping chatbot

---
+++ main.py
@@ -0,0 +1,7 @@
+print("hi im Adam")
+
+Name = input("what is your name ? ")
+movie = input("whats your favorite movie ?")
+hobby = input("what is your hobby ? ")
+Color = input("whats your favorite color ?")
+song = input("whats your favorite song ?")
